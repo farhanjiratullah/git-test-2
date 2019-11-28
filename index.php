b032b012b6c4ddaaf95fc93dbbb73d2c6cecd0a9
Farhan Jiratullah <farhanjiratullah794@gmail.com>
2019-11-28 13:15:54 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
 	<h1>Hello World!</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo odio, fuga vitae optio quasi unde quidem libero repellat culpa neque doloribus, tempora dignissimos sapiente esse sequi voluptatum, voluptas voluptatibus. Cumque.</p>
+	<ul>
+		<li>Satu</li>
+		<li>Dua</li>
+	</ul>
 </body>
 </html>
